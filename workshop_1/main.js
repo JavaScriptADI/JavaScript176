981bec96ab987d769040f3c65ea8fa625676b4c1
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2024-07-17 21:14:29 +0400

    Add workshop 1

---
+++ workshop_1/main.js
@@ -0,0 +1,32 @@
+// statment
+console.log("Hello world!"); // string
+console.log("5 + 7");
+// expression
+console.log(5 + 7); // number
+// expression
+8 + 8;
+// addition / plus
+console.log("8 + 8 =", 8 + 8);
+// substraction / minus
+console.log("8 - 7 =", 8 - 7);
+// multiplication / product
+console.log("8 * 2 =", 8 * 2);
+// divison
+console.log("8 / 2 =", 8 / 2);
+// power
+console.log("8 ** 2 =", 8 ** 2);
+// modulo
+console.log("10 % 3 =", 10 % 3);
+console.log("7 % 2 =", 7 % 2);
+console.log("8 % 2 =", 8 % 2);
+console.log("9 ** 0.5 =", 9 ** 0.5);
+console.log("9 ** 0.3 =", 9 ** (1 / 3));
+console.log("27 ** 0.3 =", 27 ** (1 / 3));
+console.log("1 / 3 =", 1 / 3);
+
+console.log("0.1 + 0.3 =", 0.1 + 0.3);
+console.log("0.1 + 0.2 =", 0.1 + 0.2);
+console.log("(10 + 20) / 100 =", (10 + 20) / 100);
+
+console.log(1 + 2 + 3);
+
