13310e8ff04f590d0c5a6dbabddcc3acd0936361
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2024-08-17 13:42:23 +0400

    Update workshop 9 code

--- workshop_9/todo/main.js
+++ workshop_9/todo/main.js
@@ -16,11 +16,53 @@ document.body.appendChild(p);
 console.log(p);
 */
 
+const todos = JSON.parse(localStorage.getItem('todos'));
 const todoForm = document.querySelector('#todo-form');
 const todoList = document.querySelector('#todo-list');
 
 console.log(todoForm);
 console.log(todoList);
+
+console.log(todos);
+
+for (const todo of todos) {
+    todo.createAt = new Date(todo.createAt);
+    createTodo(todo);
+}
+
+function updateStorage() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+    
+}
+
+function createTodo(todo) {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.addEventListener('click', (e) => {
+        // if (li.style.textDecoration === 'line-through') {
+        //     li.style.textDecoration = 'none';
+        // } else {
+        //     li.style.textDecoration = 'line-through';
+        // }
+        li.classList.toggle('done');
+        todo.done = !todo.done;
+        updateStorage();
+    });
+    
+    deleteButton.addEventListener('click', (e) => {
+        li.remove();
+        todos.splice(todos.findIndex((todo_) => todo_ === todo), 1);
+        updateStorage();
+    });
+    deleteButton.innerText = 'X';
+    // li.innerText = newTodoText;
+    if (todo.done) {
+        li.classList.add('done');
+    }
+    li.innerHTML = `<span>${todo.todo} - ${todo.createAt.toLocaleDateString()} - ${todo.createAt.toLocaleTimeString()}</span>`;
+    li.appendChild(deleteButton);
+    todoList.appendChild(li);
+}
 
 todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -31,24 +73,13 @@ todoForm.addEventListener('submit', (e) => {
     }
     const newTodoText = todoForm.todo.value;
     todoForm.todo.value = '';
-
-    const li = document.createElement('li');
-    const deleteButton = document.createElement('button');
-    li.addEventListener('click', (e) => {
-        // if (li.style.textDecoration === 'line-through') {
-        //     li.style.textDecoration = 'none';
-        // } else {
-        //     li.style.textDecoration = 'line-through';
-        // }
-        li.classList.toggle('done');
-    });
-    
-    deleteButton.addEventListener('click', (e) => {
-        li.remove();
-    });
-    deleteButton.innerText = 'X';
-    // li.innerText = newTodoText;
-    li.innerHTML = `<span>${newTodoText}</span>`;
-    li.appendChild(deleteButton);
-    todoList.appendChild(li);
+    const todo = {
+        todo: newTodoText,
+        createAt: new Date(),
+        done: false,
+    }
+    todos.push(todo);
+    createTodo(todo);
+    updateStorage();
 });
+
